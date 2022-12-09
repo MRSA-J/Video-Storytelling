6061324d76e68996da0867790b40a67eb4830e34
MRSA-J <54956912+MRSA-J@users.noreply.github.com>
2022-12-09 18:15:34 -0500

    Add image folder creator
    modify so that users don't have to manually create image folders

--- convert_video_to_image.py
+++ convert_video_to_image.py
@@ -1,19 +1,26 @@
 import cv2
 import glob
 from tqdm import tqdm
+import os
+
+VIDEO_PATH = './data/YouTubeClips/*.avi'
+
 
 '''Convert Video to Image'''
 # Input: video name
-# Output: i.e. "/images/nameimage1.jpg"
+# Output: Create a image folder and put the image generated from video to the folder
+# i.e. "/images/nameimage1.jpg"
 # name - video name, 1 - video image counter, image - a flag to symbolize it is a image
 def video_to_image(video_name):
+    if not os.path.exists("./Images"):
+        os.mkdir("./Images")
     vidcap = cv2.VideoCapture(video_name)
 
     def getFrame(sec):
         vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
         hasFrames, image = vidcap.read()
         if hasFrames:
-            cv2.imwrite("../images/" + video_name[:-4] + "image" + str(count) + ".jpg", image)
+            cv2.imwrite("./Images/" + video_name.split('\\')[-1][:-4] + "image" + str(count) + ".jpg", image)
         return hasFrames
 
     sec = 0
@@ -26,10 +33,10 @@ def video_to_image(video_name):
         sec = round(sec, 2)
         success = getFrame(sec)
 
-# When running, create a data foler.
-# Download Youtube Clips from https://www.cs.utexas.edu/users/ml/clamp/videoDescription/ and put it below "data" folder to run
 if __name__ == "__main__":
-    files = glob.glob('./data/YouTubeClips/*.avi')
+    # Usage: download dataset from https://www.cs.utexas.edu/users/ml/clamp/videoDescription/, create a data folder
+    # and put both caption & video below the data folder.
+    files = glob.glob(VIDEO_PATH)
 
     for file in tqdm(files):
         video_to_image(file)
