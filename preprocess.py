27cb6dfc2db9d539d1915c487e0bb445f0a71fde
zangyuan <zangyuan17@icloud.com>
2022-12-10 20:51:47 -0500

    add train and predict

--- preprocess.py
+++ preprocess.py
@@ -14,15 +14,17 @@ import random
 # device=torch.device('cuda:0')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 clip_model, preprocesser = clip.load("ViT-B/32", device=device, jit=False)
-CAPTION_PATH = './data/AllVideoDescriptions.txt'
-
+CAPTION_PATH = '../vit/AllVideoDescriptions.txt'
+torch.random.manual_seed(333)
+padding_embdding = torch.randn(512).unsqueeze(0)
 # Method to get image, caption list which corresponds to a single video
 # Each image is a 768 dimension vector
 # Suppose a,b,c,d is 4 image corresponds to 1 video, this method returns a [a,b,c,d] vector which concate them together and a caption list.
-def align_video_with_caption(video_name, all_captions, preprocesser):
+def align_video_with_caption(video_name, all_captions, preprocesser,padding=20,pad=False):
     images = []
-    for i in range(1,100):
-        image_file = './images/'+video_name+'image'+str(i)+'.jpg'
+    masks = [0]
+    for i in range(1,padding+1):
+        image_file = '../vit/images/'+video_name+'image'+str(i)+'.jpg'
         if not os.path.isfile(image_file):
             break
         image = preprocesser(Image.open(image_file)).unsqueeze(0).to(device)
@@ -30,26 +32,29 @@ def align_video_with_caption(video_name, all_captions, preprocesser):
         with torch.no_grad():
             image_encoding = clip_model.encode_image(image).cpu()
         images.append(image_encoding)
-
-    images = torch.cat(images, dim=0)
+        masks.append(0)
+    if pad:
+        while len(images)<padding:
+            images.append(padding_embdding)
+            masks.append(1)
+    images = torch.cat(images, dim=0).unsqueeze(0)
     captions = all_captions[video_name]
-    return images, captions
+    masks =torch.Tensor(masks).type(torch.bool).unsqueeze(0)
+    return images, captions,masks
 
 # Brute Force: Simply convert video to 1 image frame (we will analyze it in our report why this makes sense).
 def video_to_single_caption(video_name, all_captions, preprocesser):
     idx = random.randint(1,3)
-    image_file = './images/'+video_name+'image'+str(idx)+'.jpg'
+    image_file = '../vit/images/'+video_name+'image'+str(idx)+'.jpg'
 
     if not os.path.isfile(image_file):
         idx = 1
-        image_file = './images/' + video_name + 'image' + str(idx) + '.jpg'
-        image = preprocesser(Image.open(image_file)).unsqueeze(0).to(device)
-
+        image_file = '../vit/images/' + video_name + 'image' + str(idx) + '.jpg'
     image = preprocesser(Image.open(image_file)).unsqueeze(0).to(device)
 
     with torch.no_grad():
         image_encoding = clip_model.encode_image(image).cpu()
-    captions = all_captions[video_name]
+    captions = all_captions[video_name][0]
     return image_encoding, captions
 
 # The method to build a caption dictionary
@@ -63,7 +68,7 @@ def build_caption_dict():
             video_name=l[0]
             if video_name not in cap_dict:
                 cap_dict[video_name] = []
-            cap = l[1:]
+            cap = ' '.join(l[1:])+'.'
             cap_dict[video_name].append(cap)
     return cap_dict
 
@@ -74,28 +79,46 @@ def build_dataset():
     all_cap_dict = build_caption_dict()
     # One image per video
     video_single_images = []
-
+    mean_images = []
+    i=0
     for video_name in tqdm(all_cap_dict):
-        images, captions= align_video_with_caption(video_name,all_cap_dict,preprocesser)
-        all_images.append(images)
-        all_captions.append(captions)
+        images, captions,_= align_video_with_caption(video_name,all_cap_dict,preprocesser)
+        #all_images.append(images)
+        #all_captions.append(captions)
         # Create (video -> single image, caption list) dataset
-        single_image, captions = video_to_single_caption(video_name, all_cap_dict, preprocesser)
-        video_single_images.append(single_image)
 
-    assert len(all_images)==len(all_captions)
-
-    with open('save_dataset.pkl', 'wb') as fw:
-        pickle.dump((all_images, all_captions),fw)
-
-    # The method to take means of all the images
-    all_images_mean = torch.mean(torch.concat(all_images, dim=0), dim=0)
-    with open('save_dataset_mean.pkl', 'wb') as fw:
-        pickle.dump((all_images_mean, all_captions),fw)
-    # Video -> Single image, Caption List dataset
-    with open('save_dataset_single.pkl', 'wb') as fw:
-        pickle.dump((video_single_images, all_captions), fw)
-
-
+        #single_image, caption = video_to_single_caption(video_name, all_cap_dict, preprocesser)
+        #video_single_images.append(single_image)
+        d={}
+        d["clip_embedding"] = i
+        i += 1
+        d["caption"] = captions[0]
+        all_captions.append(d)
+        print(d)
+        mean_image = torch.mean(images,dim=0)
+        mean_images.append(mean_image.unsqueeze(0))
+    with open('save_dataset_mean.pkl', 'wb') as f:
+        pickle.dump({"clip_embedding": torch.cat(mean_images, dim=0), "captions": all_captions}, f)
+def build_seq_dataset():
+    all_images = []
+    all_captions = []
+    all_cap_dict = build_caption_dict()
+    i = 0
+    all_masks = []
+    for video_name in tqdm(all_cap_dict):
+        images, captions, masks= align_video_with_caption(video_name,all_cap_dict,preprocesser,padding=20,pad=True)
+        all_images.append(images)
+        all_masks.append(masks)
+        d = {}
+        d["clip_embedding"] = i
+        i += 1
+        d["caption"] = captions[0]
+        all_captions.append(d)
+    all_images=torch.cat(all_images, dim=0)
+    all_masks = torch.cat(all_masks, dim=0)
+    print(all_images.shape)
+    print(all_masks.shape)
+    with open('save_dataset_seq.pkl', 'wb') as f:
+        pickle.dump({"clip_embedding": all_images, "captions": all_captions, "masks": all_masks}, f)
 if __name__ == "__main__":
-    build_dataset()
+    build_seq_dataset()
